0a79d54020ffc16dead33805280006b7ebed9de1
Alexis-mish <elbergomeztorba424@gmail.com>
2025-05-27 21:15:55 -0700

    Se determinó las funciones de la clase borracho

---
+++ borrachitos.py
@@ -0,0 +1,23 @@
+import time
+import random
+
+baño_ocupado = False
+llamada_ocupada = False
+
+acciones = ["tomar", "cantar", "llamar_ex", "usar_baño"]
+
+class Borracho:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def tomar(self):
+        pass
+
+    def cantar(self):
+        pass
+
+    def llamar_ex(self):
+        pass
+
+    def usar_baño(self):
+        pass
